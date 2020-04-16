e25c295d2ad45adc716c539c9ab23b1656af88fa
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-16 12:35:01 +0300

    Add simple auth implementation

---
+++ src/WebApp/Pages/Sales.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    [Authorize]
+    public class SalesModel : PageModel
+    {
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+            Name = this.User.Identity.Name;
+        }
+    }
+}
--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApp
 {
@@ -25,6 +20,8 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+                .AddAzureAD(options => Configuration.Bind("AzureAd", options));
             services.AddControllers();
             services.AddRazorPages();
         }
@@ -41,6 +38,7 @@ namespace WebApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
