a3971ae252abe90adf30f3d70f0055f6b2683190
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-18 09:16:34 +0300

    Change the comment to match functionality

--- src/WebApp/Pages/Products.cshtml.cs
+++ src/WebApp/Pages/Products.cshtml.cs
@@ -5,8 +5,8 @@ namespace WebApp.Pages
 {
     [Authorize]
     // Or then directly validate the claim via policy.
-    // NOTE: In this example it will redirect to non-existing access denied page:
-    //       "/Account/AccessDenied?ReturnUrl=%2Fproducts"
+    // NOTE: In this example it will redirect to access denied page:
+    //       "/AccessDenied?ReturnUrl=%2Fproducts"
     // [Authorize("CustomClaim")]
     public class ProductsModel : PageModel
     {
